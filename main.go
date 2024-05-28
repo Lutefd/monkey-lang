11e0f1f58a1fe04356e99f096f3d31da197f75cf
Luis Dourado <lfaddourado@gmail.com>
2024-05-27 22:19:50 -0300

    feat(main): add debug statements on main for hands-on testing

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/lutefd/monkey-lang/lexer"
+)
+
+func main () {
+	testStr := "test ==	 aa"
+	l := lexer.New(testStr)
+	testFields := strings.Fields(testStr)
+	for _, test := range testFields{
+		l.NextToken()
+		fmt.Println(test)
+	}
+}
